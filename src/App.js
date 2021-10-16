62004e3160e312775c66851c05c3e8944d10d8ba
Sal Ferrarello <5194588+salcode@users.noreply.github.com>
2021-10-16 14:58:22 -0400

    Add muppetsData
    muppetsData is a nested JavaScript object with information about
    muppets, specifically the performers who played the Muppet and when

--- src/App.js
+++ src/App.js
@@ -1,5 +1,69 @@
 import './App.css';
 
+const muppetsData = [
+  {
+    name: 'Fozzie Bear',
+    performers: [
+      {
+        name: 'Frank Oz',
+        startYear: 1976,
+        endYear: 2000,
+      },
+      {
+        name: 'Eric Jacobson',
+        startYear: 2002,
+      },
+    ],
+  },
+  {
+    name: 'Gonzo',
+    performers: [
+      {
+        name: 'Dave Goelz',
+        startYear: 1976,
+      },
+    ],
+  },
+  {
+    name: "Kermit the Frog",
+    performers: [
+      {
+        name: 'Matt Vogel',
+        startYear: 2017,
+      },
+      {
+        name: 'Steve Whitmire',
+        startYear: 1990,
+        endYear: 2016,
+      },
+      {
+        name: 'Jim Henson',
+        startYear: 1955,
+        endYear: 1990,
+      },
+    ],
+  },
+  {
+    name: 'Sam Eagle',
+    performers: [
+      {
+        name: 'Frank Oz',
+        startYear: 1976,
+        endYear: 2000,
+      },
+      {
+        name: 'Kevin Clash',
+        startYear: 2002,
+        endYear: 2003,
+      },
+      {
+        name: 'Eric Jacobson',
+        startYear: 2005,
+      },
+    ],
+  },
+];
+
 function App() {
   return (
     <div className="App">
