216b9dfee5c34b3ca3019fb423d9ebe650151efe
Sal Ferrarello <5194588+salcode@users.noreply.github.com>
2021-10-16 14:26:56 -0400

    Remove extraneous markup and CSS

--- src/App.js
+++ src/App.js
@@ -4,17 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>ReactJS Starter</h1>
       </header>
     </div>
   );
