829f245207f306c17888deafd1ed934010739038
Sal Ferrarello <5194588+salcode@users.noreply.github.com>
2021-10-18 20:26:28 -0400

    Add useEffect to decrement counter to zero
    Add useEffect to decrement counter to zero, reducing the value by 1
    every 500 ms

--- src/components/Countdown/index.js
+++ src/components/Countdown/index.js
@@ -1,9 +1,23 @@
 import {
+  useEffect,
   useState,
 } from 'react';
 
 export default function Countdown() {
   const [counter, setCounter] = useState(20);
+
+  useEffect(
+    () => {
+      if (counter <= 0) {
+        return;
+      }
+      setTimeout(
+        () => setCounter(counter - 1),
+        500
+      );
+    },
+    [counter]
+  );
 
   return (
     <h2>{counter}</h2>
