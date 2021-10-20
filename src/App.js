8c222c1a6cf6010fce21dd43d2e22ab748e8d96f
Sal Ferrarello <5194588+salcode@users.noreply.github.com>
2021-10-20 19:27:26 -0400

    Add RedlightGreenlight component to App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import { useState } from 'react';
 
+import RedlightGreenlight from './components/RedlightGreenlight';
+
 function App() {
   const [go, setGo] = useState(false);
   return (
@@ -8,6 +10,7 @@ function App() {
       <header className="App-header">
         <h1>ReactJS Starter</h1>
       </header>
+      <RedlightGreenlight />
       <footer className="App-footer">
         <p>Starter created by <a href="https://github.com/salcode">salcode</a></p>
       </footer>
---
+++ src/components/RedlightGreenlight/index.js
@@ -0,0 +1,8 @@
+export default function RedlightGreenlight() {
+  return (
+    <>
+      <h2>RedlightGreenlight</h2>
+      <h3>stop/go</h3>
+    </>
+  );
+}
