6ef2347967604dcb929532d6454f0ebb29016041
Sal Ferrarello <5194588+salcode@users.noreply.github.com>
2021-10-29 10:47:24 -0400

    Add useEffect() which will run once (on mount)
    Add useEffect() to RemoteApiBlogPosts component which will run once (on mount)
    
    This useEffect() only runs once because no values are listed in the
    dependencies array (the second parameter ([])).

--- src/components/RemoteApiBlogPosts/index.js
+++ src/components/RemoteApiBlogPosts/index.js
@@ -1,4 +1,17 @@
+import {
+  useEffect,
+} from 'react';
+
 export default function RemoteApiBlogPosts() {
+
+  useEffect(() => {
+    async function fetchData() {
+      console.log('will async fetch data on component mount');
+    }
+
+    fetchData();
+  }, []);
+
   return (
     <h2>RemoteApiBlogPosts stub</h2>
   );
