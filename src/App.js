9be8cf55f3b20fafaa94b282c34091c9d1b0bacc
Sal Ferrarello <5194588+salcode@users.noreply.github.com>
2021-10-29 10:45:43 -0400

    Add stub RemoteApiBlogPosts component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 
+import RemoteApiBlogPosts from './components/RemoteApiBlogPosts';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>ReactJS Starter</h1>
       </header>
+      <RemoteApiBlogPosts />
       <footer className="App-footer">
         <p>Starter created by <a href="https://github.com/salcode">salcode</a></p>
       </footer>
---
+++ src/components/RemoteApiBlogPosts/index.js
@@ -0,0 +1,5 @@
+export default function RemoteApiBlogPosts() {
+  return (
+    <h2>RemoteApiBlogPosts stub</h2>
+  );
+}
