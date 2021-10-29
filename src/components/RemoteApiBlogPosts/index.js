ab39f5caf9e26c3cb56955f4f26b3f735b318a32
Sal Ferrarello <5194588+salcode@users.noreply.github.com>
2021-10-29 10:50:15 -0400

    Add code to fetch remote data and console.log
    Currently, the data we fetch is only displayed in the browser console

--- src/components/RemoteApiBlogPosts/index.js
+++ src/components/RemoteApiBlogPosts/index.js
@@ -6,7 +6,9 @@ export default function RemoteApiBlogPosts() {
 
   useEffect(() => {
     async function fetchData() {
-      console.log('will async fetch data on component mount');
+      const result = await fetch('https://salferrarello.com/wp-json/wp/v2/posts/');
+      const newBlogPosts = await result.json();
+      console.log(newBlogPosts);
     }
 
     fetchData();
