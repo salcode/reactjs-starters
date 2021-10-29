4617e35a4169fa6b500c9ecfa457847e022b8a3f
Sal Ferrarello <5194588+salcode@users.noreply.github.com>
2021-10-29 10:51:23 -0400

    Store fetched blog posts in state
    At this point we have our data in state but are not yet displaying it.

--- src/components/RemoteApiBlogPosts/index.js
+++ src/components/RemoteApiBlogPosts/index.js
@@ -1,14 +1,17 @@
 import {
   useEffect,
+  useState,
 } from 'react';
 
 export default function RemoteApiBlogPosts() {
+  const [blogPosts, setBlogPosts] = useState([]);
 
   useEffect(() => {
     async function fetchData() {
       const result = await fetch('https://salferrarello.com/wp-json/wp/v2/posts/');
       const newBlogPosts = await result.json();
       console.log(newBlogPosts);
+      setBlogPosts(newBlogPosts);
     }
 
     fetchData();
